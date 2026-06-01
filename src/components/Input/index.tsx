e3bdb8c32e5d8b2cfa638653113d30674a003dc7
dhanglersondev <dhanglerson.dev@outlook.com>
2026-06-01 20:24:40 -0300

    Componentes de input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,39 @@
+import type {
+  ChangeEvent,
+  InputHTMLAttributes,
+} from "react";
+
+interface InputProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  value?: string;
+  onChange?: (
+    event: ChangeEvent<HTMLInputElement>
+  ) => void;
+}
+
+export function Input({
+  value,
+  onChange,
+  ...props
+}: InputProps) {
+  return (
+    <input
+      className="
+        w-full
+        rounded-md
+        bg-gray-200
+        px-4
+        py-3
+        text-gray-700
+        outline-none
+        transition-all
+        placeholder:text-gray-500
+        focus:ring-2
+        focus:ring-purple-500
+      "
+      value={value}
+      onChange={onChange}
+      {...props}
+    />
+  );
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,68 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { useState, type FormEvent } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center px-4 py-6">
+      <Link to="/">
+        <h1 className="text-center text-4xl font-bold text-white sm:text-5xl">
+          Dev
+          <span className="bg-linear-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+            Link
+          </span>
+        </h1>
+      </Link>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-6 flex w-full max-w-xl flex-col gap-3"
+      >
+        <Input
+          placeholder="Seu email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="********"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          className="
+            w-full
+            rounded-md
+            bg-purple-500
+            px-4
+            py-2.5
+            font-semibold
+            text-white
+            transition-colors
+            hover:bg-purple-600
+            active:scale-[0.98]
+            cursor-pointer
+          "
+        >
+          Acessar
+        </button>
+      </form>
     </div>
   );
 }
