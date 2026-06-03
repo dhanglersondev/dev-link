609cadf408ceea74ffc6e39b4e29e6b14b1eb9c5
dhanglersondev <dhanglerson.dev@outlook.com>
2026-06-03 14:46:23 -0300

    Exibindo midias e links

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,35 +1,117 @@
+import { useEffect, useState } from 'react';
 import { Social } from '../../components/Social';
 import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { collection, doc, getDoc, getDocs, orderBy, query } from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection';
+
+interface LinkProps {
+  id: string,
+  titulo: string,
+  url: string,
+  bg: string,
+  color: string
+}
+
+interface SocialLinksProps {
+  facebook: string,
+  instagram: string,
+  linkedin: string,
+  github: string
+}
 
 export function Home() {
+  const [links, setLinks] = useState<LinkProps[]>([])
+  const [socialLinks, getSocialLinks] = useState<SocialLinksProps>()
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, "links")
+      const queryRef = query(linksRef, orderBy("created", "asc"))
+
+      getDocs(queryRef)
+        .then((snapshot) => {
+          let lista = [] as LinkProps[];
+
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              titulo: doc.data().titulo,
+              url: doc.data().url,
+              bg: doc.data().bg,
+              color: doc.data().color,
+            })
+          })
+
+          setLinks(lista)
+        })
+    }
+
+    loadLinks();
+  }, [])
+
+  useEffect(() => {
+    function loadSocialLinks() {
+      const docRef = doc(db, "social", "link");
+
+      getDoc(docRef)
+        .then((snapshot) => {
+          if (!snapshot.exists()) return;
+
+          const data = snapshot.data();
+
+          getSocialLinks({
+            facebook: data.facebook ?? "",
+            instagram: data.instagram ?? "",
+            linkedin: data.linkedin ?? "",
+            github: data.github ?? ""
+          });
+        })
+        .catch((error) => {
+          console.error("Erro ao carregar links:", error);
+        });
+    }
+
+    loadSocialLinks();
+  }, []);
+
   return (
     <div className="flex flex-col w-full py-6 px-4 items-center justify-center">
-      <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Sujeito Programador</h1>
+      <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Dhanglerson Dev</h1>
       <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
-        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-          <a>
-            <p className="text-base md:text-lg">
-              Canal no Youtube
-            </p>
-          </a>
-        </section>
+        {links.map((link) => (
+          <section
+            style={{ backgroundColor: link.bg }}
+            key={link.id}
+            className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+            <a href={link.url} target='_blank'>
+              <p className="text-base md:text-lg" style={{ color: link.color }}>
+                {link.titulo}
+              </p>
+            </a>
+          </section>
+        ))}
 
-        <footer className="flex justify-center gap-3 my-4">
-          <Social url="https://www.facebook.com/dhanglerson14/">
-            <FaFacebook size={24} color="#fff" />
-          </Social>
-          <Social url="https://www.instagram.com/dhanglerson14/">
-            <FaInstagram size={24} color="#fff" />
-          </Social>
-          <Social url="https://www.linkedin.com/in/dhanglerson-barral-b04366409/">
-            <FaLinkedin size={24} color="#fff" />
-          </Social>
-          <Social url="https://www.facebook.com/profile.php?id=61588341800259">
-            <FaGithub size={24} color="#fff" />
-          </Social>
-        </footer>
+        {socialLinks && Object.keys(socialLinks).length > 0 && (
+          <footer className="flex justify-center gap-3 my-4">
+            <Social url={socialLinks.facebook}>
+              <FaFacebook size={24} color="#fff" />
+            </Social>
+
+            <Social url={socialLinks.instagram}>
+              <FaInstagram size={24} color="#fff" />
+            </Social>
+
+            <Social url={socialLinks.linkedin}>
+              <FaLinkedin size={24} color="#fff" />
+            </Social>
+
+            <Social url={socialLinks.github}>
+              <FaGithub size={24} color="#fff" />
+            </Social>
+          </footer>
+        )}
       </main>
     </div>
   )
